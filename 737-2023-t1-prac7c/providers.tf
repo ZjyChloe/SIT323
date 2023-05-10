57ca2866bd212038964692c198ef61bbd8a98a3b
ZjyChloe <zjychloe32@gmail.com>
2023-05-11 01:02:55 +1000

    new file:   737-2023-t1-prac7c/container-registry.tf 	new file:   737-2023-t1-prac7c/database.tf 	new file:   737-2023-t1-prac7c/kubernetes-cluster.tf 	new file:   737-2023-t1-prac7c/private-key.tf 	new file:   737-2023-t1-prac7c/providers.tf 	new file:   737-2023-t1-prac7c/resource-group.tf 	new file:   737-2023-t1-prac7c/variables.tf 	new file:   737-2023-t1-prac7c/video-streaming.tf 	new file:   737-2023-t1-prac7c/video-streaming/Dockerfile-dev 	new file:   737-2023-t1-prac7c/video-streaming/Dockerfile-prod 	new file:   737-2023-t1-prac7c/video-streaming/package-lock.json 	new file:   737-2023-t1-prac7c/video-streaming/package.json 	new file:   737-2023-t1-prac7c/video-streaming/src/index.js 	new file:   737-2023-t1-prac7c/video-streaming/videos/SampleVideo_1280x720_1mb.mp4

---
+++ 737-2023-t1-prac7c/providers.tf
@@ -0,0 +1,24 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+provider "kubernetes" {
+    version = "1.10.0"
+    host = azurerm_kubernetes_cluster.cluster.kube_config[0].host
+
+    client_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.cluster.kube_config[0].cluster_ca_certificate)
+}
---
+++ 737-2023-t1-prac7c/variables.tf
@@ -0,0 +1,24 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+    default = "task7"
+}
+variable location {
+  default = "West US"
+}
+
+variable admin_username {
+  default = "linux_admin"
+}
+
+variable app_version { 
+  default = "1.26.3"
+}
+
+variable client_id {
+  default = "5df8d048-b537-4b03-9f7e-0882fe58e80b"
+}
+
+variable client_secret {
+  default = "MI68Q~od4kgVB.IOVI~eozWO7GZBGSEJa4eXdal6"
+}
